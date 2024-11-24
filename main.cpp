db1472247a813983780da7e9231b6bdf3482343e
Jan Filip <filiphanz@gmail.com>
2024-11-24 10:59:33 +0100

    wrong format for lint testing

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
+#include <vector>// comment without space
 #include <iostream>
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+    std::cout<<"Hello, World!" << std::endl;
+    return 0;  
 }
