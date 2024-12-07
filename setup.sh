9884fbdabf5e4aeda6e1f3cf5f05b783ea3c1ed6
root <root@DESKTOP-UFHV2A9>
2024-12-07 13:36:58 +0100

    modify setup.sh

--- setup.sh
+++ setup.sh
@@ -1,2 +1,6 @@
+#!/bin/bash
+
 conan install . --output-folder=build --build=missing
-cmake -S . -B build
+
+cmake -S . -B build -D CMAKE_TOOLCHAIN_FILE='build/build/Release/generators/conan_toolchain.cmake' -DCMAKE_BUILD_TYPE=Release 
+cmake --build build
