aebde74d022eb86397123e6519b088cc1231c37f
Jan Filip <filiphanz@gmail.com>
2024-12-19 21:09:48 +0100

    extend linux setup

--- setup.sh
+++ setup.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
-
 conan install . --output-folder=build --build=missing
 
-cmake -S . -B build -D CMAKE_TOOLCHAIN_FILE='build/build/Release/generators/conan_toolchain.cmake' -DCMAKE_BUILD_TYPE=Release 
-cmake --build build
+mkdir -p build
+cd build
+
+cmake .. -DCMAKE_TOOLCHAIN_FILE=conan_toolchain.cmake
+cmake --build .
