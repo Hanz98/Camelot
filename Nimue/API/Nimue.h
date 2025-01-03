a3ea024c6d07421315f5b189ed814a6a97379441
Jan Filip <filiphanz@gmail.com>
2025-01-03 16:23:39 +0100

    adding basic Nimue architecture

---
+++ Nimue/API/Nimue.h
@@ -0,0 +1,26 @@
+/*
+ * Copyright 2024 Jan Filip
+ *
+ * Licensed under the MIT License. You may not use this file except in
+ * compliance with the License. You may obtain a copy of the License at
+ *
+ * https://opensource.org/licenses/MIT
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef NIMUE_API_NIMUE_H_
+#define NIMUE_API_NIMUE_H_
+
+#include <iostream>
+
+class MainModel {
+ public:
+  void test() { std::cout << "Hello World from Nimue!" << std::endl; }
+};
+
+#endif  // NIMUE_API_NIMUE_H_
